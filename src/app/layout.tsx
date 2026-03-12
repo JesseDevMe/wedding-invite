34507c04b9f57bffdc024bd9075b53b2f6a00156
JesseMe <dev.jesseme@gmail.com>
2026-03-12 16:33:57 +0300

    chore/no index by browsers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,14 @@ const birthstoneBounce = Birthstone_Bounce({
 export const metadata: Metadata = {
   title: "Приглашение на свадьбу",
   description: "Приглашаем вас на нашу свадьбу",
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
 };
 
 export default function RootLayout({
