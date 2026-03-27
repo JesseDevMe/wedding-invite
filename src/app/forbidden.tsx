10964e30aba41ee917317caefb38d61da6260204
JesseMe <dev.jesseme@gmail.com>
2026-03-28 00:20:19 +0300

    style

--- src/app/forbidden.tsx
+++ src/app/forbidden.tsx
@@ -4,9 +4,15 @@ import { Contacts } from "@/features/contacts";
 export default function Forbidden() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-2xl font-bold">Доступ запрещен</h2>
-      <p className="text-lg">Копируйте отправленную вам ссылку целиком</p>
-      <p className="text-lg">Если ошибка повторяется, обратитесь к Никитуле</p>
+      <div className="flex flex-col items-center justify-center px-4 pt-10">
+        <h2 className="text-2xl font-bold">Доступ запрещен</h2>
+        <p className="text-lg text-center mt-4 ">
+          Копируйте отправленную вам ссылку целиком
+        </p>
+        <p className="text-lg text-center">
+          Если ошибка повторяется, обратитесь к Никитуле
+        </p>
+      </div>
       <Contacts />
     </div>
   );
